0897c3b1ab3ad44107e40cd84e2fd1505fc8f6d8
haenno <haenno@web.de>
2022-08-30 01:11:10 +0200

    fix start and CRLF/LF error with start_api.sh

--- 50-docker-deployment/ai_api_backend/start_api.sh
+++ 50-docker-deployment/ai_api_backend/start_api.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-echo 'Django startup...'
+echo 'Django startup... '
 pipenv run python manage.py makemigrations
 pipenv run python manage.py migrate --run-syncdb  # Apply database migrations
 pipenv run python manage.py collectstatic --noinput  # Collect static files
 pipenv run python -m chatbot -t  # train AI model
 
-echo 'Starting Django in production mode (Daphne)...'
+echo 'Starting Django in production mode (Daphne)... '
 exec pipenv run daphne -b 0.0.0.0 -p 8000 aiapiproject.asgi:application
